79462eb26cbcdf3166637fe6246d8cacb307564c
Denis <beznikind@gmail.com>
2023-01-15 11:58:35 +0300

    Квадрат числа

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа 
+
+Console.Write("Введите число возводимое в квадрат - ");
+int numberA = Int32.Parse(Console.ReadLine());
+int result = (numberA * numberA);
+Console.Write("Ответ - ");
+Console.WriteLine(result);
