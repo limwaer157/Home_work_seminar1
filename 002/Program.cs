e23aa253d198e3af7eff957f22ed84cfe9d585d0
Denis <beznikind@gmail.com>
2023-01-15 13:18:52 +0300

    По двум заданным числам проверять является ли первое квадратом второго

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int numberB = Int32.Parse(Console.ReadLine());
+int numberC = (numberB * numberB);
+
+  if(numberA == numberC)
+   {
+     Console.Write(" Число - ");
+     Console.Write(numberA);
+     Console.Write(" является квадратом первого ");
+    } 
+   else
+   {
+     Console.Write(" Число -  ");
+     Console.Write(numberA);
+     Console.Write(" не является квадратом первого ");
+   }
