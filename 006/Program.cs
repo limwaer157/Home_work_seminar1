940e76d38840df669e914b6b446a17614674d0c7
Denis <beznikind@gmail.com>
2023-01-15 15:20:56 +0300

    Показать четные числа от 1 до N

---
+++ 006/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Показать четные числа от 1 до -  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int numbers = 1;
+
+while(numbers <= n)
+{
+    if(numbers % 2 == 0)
+    {
+        Console.WriteLine(numbers);
+        numbers = numbers + 2;
+    }
+    else
+    {
+        numbers = numbers + 1;
+    }
+}
