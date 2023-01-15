de2adbb490e993fded41f74bf0916b1b2e37c9ec
Denis <beznikind@gmail.com>
2023-01-15 15:51:11 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ 007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите  число - ");
+int Number = Int32.Parse(Console.ReadLine());
+int lastNumber = Number % 10;
+Console.Write("Последяя цифра - ");
+Console.Write(lastNumber);
