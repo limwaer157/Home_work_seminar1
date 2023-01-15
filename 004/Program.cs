584add4949f0d26492b8d435913d68aa6550e369
Denis <beznikind@gmail.com>
2023-01-15 14:34:52 +0300

    Выяснить является ли число чётным

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из трех чисел
+
+
+Console.Write("Введите первое число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int numberB = Int32.Parse(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int numberС = Int32.Parse(Console.ReadLine());
+int maxNumber = numberA;
+
+if(numberA > numberB )
+     {
+    maxNumber = numberA;
+     } 
+ if(numberB > numberA )
+     {
+    maxNumber = numberB;
+     } 
+if(numberC > numberB)
+     {
+     maxNumber = numberC;
+     } 
+  Console.Write("Наибольшее число - ");
+  Console.Write(maxNumber);
---
+++ 005/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите  число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+
+int numberC = (numberA % 2 );
+
+if(numberC == 1 )
+     {
+     Console.Write(numberA);
+     Console.Write(" - число четное");
+     } 
+     else
+     {
+        Console.Write(numberA);
+        Console.Write(" - число четное");
+     }
+
+
