5bd882cc3f5ccb347062c33c131f69ccc66b5e3c
Denis <beznikind@gmail.com>
2023-01-15 13:49:36 +0300

    По заданному номеру дня недели вывести его название

---
+++ 003/Program.cs
@@ -0,0 +1,34 @@
+﻿ // По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели от 1 до 7 - ");
+int numberA = Int32.Parse(Console.ReadLine());
+
+  if(numberA == 1 )
+     {
+     Console.Write(" Понедельник ");
+     } 
+ if(numberA == 2 )
+     {
+     Console.Write(" Вторник ");
+     } 
+ if(numberA == 3 )
+     {
+     Console.Write(" Среда ");
+     } 
+ if(numberA == 4 )
+     {
+     Console.Write(" Четверг ");
+     } 
+ if(numberA == 5 )
+     {
+     Console.Write(" Пятница ");
+     } 
+ if(numberA == 6 )
+     {
+     Console.Write(" Суббота ");
+     } 
+ if(numberA == 7 )
+     {
+     Console.Write(" Воскресенье ");
+     } 
+  
