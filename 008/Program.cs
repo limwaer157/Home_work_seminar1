af716c7766aa7036557a23d6c84a15014b779fc1
Denis <beznikind@gmail.com>
2023-01-15 16:14:17 +0300

    Показать вторую цифру трёхзначного числа

---
+++ 008/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите  число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+
+string string1 = numberA.ToString();
+Console.Write("Вторая  цифра введеного числа - ");
+Console.Write(string1[1]);
