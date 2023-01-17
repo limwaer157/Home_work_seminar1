9bd00c56a7ef65521d6f2d251955852c8b79118a
Denis <beznikind@gmail.com>
2023-01-17 18:31:32 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 009/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите  число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+string string1 = numberA.ToString();
+Console.Write("Вторая  цифра введеного числа - ");
+
+index.ToDelete string1[2];
+Console.Write();
---
+++ 010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+int number = new Random().Next(10, 99);
+Console.WriteLine("Сгенерированное число - ");
+Console.WriteLine(number);
+string string1 = number.ToString();
+
+int numberA1 = Int32.Parse(string1[0].ToString());
+int numberB2 = Int32.Parse(string1[1].ToString());
+
+int max = numberB2;
+if (numberA1 > numberB2 )
+{
+    max = numberA1;
+}
+else 
+{
+numberB2 = max;
+}
+Console.WriteLine(max);
