38e3f42d27724d6cdbef60a9f2208dc972a03c91
Denis <beznikind@gmail.com>
2023-01-17 22:37:09 +0300

    найти третью цифру числа или сообщить, что её нет

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите трехзначное число число - ");
+int number = Int32.Parse(Console.ReadLine());   
+ 
+ if (number < 100)
+ {
+        Console.WriteLine("Слишком маленькое число");
+ }
+    
+    else
+    {
+        while (number > 999 )
+{
+    number = number - 1000;
+}
+if (number <= 999)
+{
+    number = number / 100;
+}
+Console.WriteLine(number);
+    }
+ 
+
+
+ 
