18dad5eeb228f5245508692111ea8996c20763ad
Denis <beznikind@gmail.com>
2023-01-17 18:54:11 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ 011/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите  число - ");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.Write("Введите  число  на которое нужно делить - ");
+int numberB = Int32.Parse(Console.ReadLine());
+
+int numberC = (numberA % numberB );
+
+if(numberC == 0 )
+     {
+     int numberD = (numberA / numberB );
+     Console.Write("Число кратно заданному ответ - ");
+     Console.Write(numberD);
+     } 
+     else
+     {
+        Console.Write(numberC);
+            }
